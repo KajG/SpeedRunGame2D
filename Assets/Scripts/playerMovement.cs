a3d10d5820c330d15cf9345da6d7cb51c03b9338
KajG <kaaiman99@hotmail.com>
2016-06-06 13:53:59 +0200

    Jumping Script

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    public float jumpHeight;
+    private float timer = 0.75f;
+    private bool Ground;
+    private bool timerCheck;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Ground)
+        {
+            _rigidbody.gravityScale = 1;
+            if (Input.GetKeyDown("i"))
+            {
+                timerCheck = true;
+                _rigidbody.AddForce(transform.up * jumpHeight);
+                Ground = false;
+            }
+        }
+        Debug.Log(timer);
+        if (timerCheck)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(timer < 0)
+        {
+            _rigidbody.gravityScale = 2;
+            timerCheck = false;
+            timer = 0.75f;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Ground = true;
+        }
+    }
+}
